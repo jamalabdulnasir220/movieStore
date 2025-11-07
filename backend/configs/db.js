ab817c7daeb20c9f704e08c509fec936de9ee880
unknown <jamalnasirone52@gmail.com>
2025-11-07 16:20:02 +0000

    update

---
+++ backend/configs/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log("Database connected successfully");
+    });
+    await mongoose.connect(`${process.env.MONGODB_URI}/showqueue`);
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+  }
+};
+
+export default connectDB;
