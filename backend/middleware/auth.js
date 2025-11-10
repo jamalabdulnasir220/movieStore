402372860c73b454c8087dbddb94a876e874f152
unknown <jamalnasirone52@gmail.com>
2025-11-10 18:39:17 +0000

    update

---
+++ backend/middleware/auth.js
@@ -0,0 +1,19 @@
+import { clerkClient } from "@clerk/express";
+
+export const protectAdmin = async (req, res, next) => {
+  try {
+    const { userId } = req.auth();
+
+    const user = await clerkClient.users.getUser(userId);
+
+    if (user.privateMetadata.role !== "admin") {
+      return res
+        .status(403)
+        .json({ success: false, message: "Access denied. Admins only." });
+    }
+    next();
+  } catch (error) {
+    console.error("Error in admin protection middleware:", error);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
