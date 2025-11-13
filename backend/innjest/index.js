1c047d078fe2c7946fc65dcf44cfc72d48d994fd
unknown <jamalnasirone52@gmail.com>
2025-11-13 16:17:14 +0000

    update

--- backend/innjest/index.js
+++ backend/innjest/index.js
@@ -1,55 +1,83 @@
 import { Inngest } from "inngest";
 import User from "../models/user.js";
+import Booking from "../models/booking.js";
+import Show from "../models/show.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
-
 // Inngest function to store user to a database
 const syncUserCreation = inngest.createFunction(
-    { id: 'sync-user-from-clerk' },
-    { event: "clerk/user.created" },
-    async ({ event }) => {
-        const { id, first_name, last_name, email_addresses, image_url } = event.data;
-        const userData = {
-            _id: id,
-            email: email_addresses[0].email_address,
-            name: first_name + " " + last_name,
-            image: image_url
-        }
-        // Here, you would add code to save userData to your database
-        await User.create(userData)
-    }
-)
+  { id: "sync-user-from-clerk" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: first_name + " " + last_name,
+      image: image_url,
+    };
+    // Here, you would add code to save userData to your database
+    await User.create(userData);
+  }
+);
 
 // Inngest function to delete user from database when deleted in Clerk
 const syncUserDeletion = inngest.createFunction(
-    { id: 'delete-user-with-clerk' },
-    { event: "clerk/user.deleted" },
-    async ({ event }) => {
-        const { id } = event.data;
-        // Here, you would add code to delete the user from your database
-        await User.findByIdAndDelete(id)
-    }
-)
+  { id: "delete-user-with-clerk" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    const { id } = event.data;
+    // Here, you would add code to delete the user from your database
+    await User.findByIdAndDelete(id);
+  }
+);
 
 // Inngest function to update user info in database when updated in Clerk
 const syncUserUpdate = inngest.createFunction(
-    { id: 'update-user-from-clerk' },
-    { event: "clerk/user.updated" },
-    async ({ event }) => {
-        const { id, first_name, last_name, email_addresses, image_url } = event.data;
-        const updatedData = {
-            _id: id,
-            name: first_name + " " + last_name,
-            email: email_addresses[0].email_address,
-            image: image_url
-        }
-        // Here, you would add code to update the user in your database
-        await User.findByIdAndUpdate(id, updatedData)
-    }
-)
+  { id: "update-user-from-clerk" },
+  { event: "clerk/user.updated" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const updatedData = {
+      _id: id,
+      name: first_name + " " + last_name,
+      email: email_addresses[0].email_address,
+      image: image_url,
+    };
+    // Here, you would add code to update the user in your database
+    await User.findByIdAndUpdate(id, updatedData);
+  }
+);
 
+// Inngest function to cancel booking and release seats of shows if after 10 minutes of creating booking, payment is not made.
+const releaseSeatsandDeleteBooking = inngest.createFunction(
+  { id: "release-seats-delete-booking" },
+  { event: "app/checkPayment" },
+  async ({ event, step }) => {
+    const tenMinutesLater = new Date(Date.now() + 10 * 60 * 1000);
+    await step.sleepUntil("wait-for-10-minutes", tenMinutesLater);
+
+    await step.run("check-payment-status", async () => {
+      const bookingId = event.data.bookingId;
+      const booking = await Booking.findById(bookingId);
+
+      //   If payment is not made, release the seats and delete the booking
+      if (!booking.isPaid) {
+        const show = await Show.findById(booking.show);
+        booking.bookedSeats.forEach((seat) => {
+          delete show.occupiedSeats[seat];
+        });
+        show.markModified("occupiedSeats");
+        await show.save();
+        await Booking.findByIdAndDelete(booking._id);
+      }
+    });
+  }
+);
 
 // Create an empty array where we'll export future Inngest functions
-export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdate];
+export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdate, releaseSeatsandDeleteBooking];
