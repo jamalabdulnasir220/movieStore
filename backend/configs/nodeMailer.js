32d912f50181a70b0b0c226c96765d2eee3e0fd5
unknown <jamalnasirone52@gmail.com>
2025-11-14 17:24:37 +0000

    update

--- backend/configs/nodeMailer.js
+++ backend/configs/nodeMailer.js
@@ -11,14 +11,17 @@ const transporter = nodemailer.createTransport({
 });
 
 const sendEmail = async ({ to, subject, body }) => {
-  const response = await transporter.sendMail({
-    from: process.env.SMTP_USER,
-    to,
-    subject,
-    html: body,
-  });
-  return response;
+  try {
+    const response = await transporter.sendMail({
+      from: process.env.SMTP_USER,
+      to,
+      subject,
+      html: body,
+    });
+    return response;
+  } catch (error) {
+    console.log(`This is an error i have been dealing with ${error.message}`);
+  }
 };
-
 
 export default sendEmail;
